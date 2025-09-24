701142d4f6ecf14b7041272c1513c87b02f1f4cd
RYU7736 <ryu.gumi.7736@icloud.com>
2025-09-25 04:00:28 +0900

    easyocrの導入とそれにともなってrequirements.txtのバージョンを変更

--- backend/ocr.py
+++ backend/ocr.py
@@ -1,28 +1,33 @@
 from flask import Blueprint,jsonify
 from pdf2image import convert_from_bytes
-from paddleocr import PaddleOCR
+import easyocr
 import numpy as np
 #以下のルーティングは開発後半で、、まずは/uploadsに統合して管理する。
 # ocr_bp = Blueprint("ocr",__name__)
 
-ocr_model = PaddleOCR(lang= "japan", use_angle_cls = True)
+ocr_model = easyocr.Reader(["ja","en"])
+
 # @ocr_bp.route("/ocr", methods = ["POST"])
 def ocr(files):
     results = []
     for f in files:
+        if not f:
+            print(f"空ファイルをスキップしました。")
+            continue
         pdf_bytes = f.read()
         if not pdf_bytes:
-            return "f.read()失敗"
+            print("f.read()失敗")
+            continue
+
         imgs = convert_from_bytes(pdf_bytes, dpi =200, fmt = "png")
 
         for img in imgs:
             img_array = np.array(img)
-            ocr_result = ocr_model.ocr(img_array)
-            page_texts = []
-            for line in ocr_result[0]:
-                text = line[1][0]
-                page_texts.append(text)
-                print(page_texts)
+            ocr_result = ocr_model.readtext(img_array)
+            page_text = []
+            for detection in ocr_result:
+                bbox, text, confidence = detection
+                page_text.append(text)
+            results.append(page_text)
 
-            results.append(page_texts)
     return results
